877bafbb20c0d4bff69898f70b589235eb68d969
yashkathirvel <yashwant1999@gmail.com>
2022-07-23 23:46:20 -0400

    updating main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # import libraries:
 
 import csv
+from os import sep
+import re
+import string
 
 # ---------------------------------------------------------------------------------
 
@@ -16,4 +19,27 @@ with open('./periodic-table.csv', 'r') as file:
         new_data = {row['Symbol'] : float(row['AtomicMass'])}
         data.update(new_data)
 
-print(data['C'])
+# user input and separate string:
+
+form = 'C10H15N'
+
+# regex that separates strings at each uppercase letter:
+
+sep_form = re.sub(r"([A-Z])", 
+                  r" \1", 
+                  form).split()
+
+# map and function that adds 1 to unnumbered elements:
+
+fixed_form = list(map(lambda x:  x + '1' if x.isalpha()==True else x,
+                      sep_form))
+
+for i in fixed_form:
+    match = re.match(r"([a-z]+)([0-9]+)", i, re.I)
+    if match:
+        items = match.groups()
+        print(items[0])
+
+
+
+
